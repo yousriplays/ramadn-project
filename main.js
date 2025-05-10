2dc715e7b9e1bcfd90348526a4fab2204eb6a4eb
yousriplays <73823875+yousriplays@users.noreply.github.com>
2025-05-10 23:38:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var ramadan = document.getElementById("ramadan");
+var crescent = document.getElementById("crescent");
+
+document.addEventListener("scroll", (e) => {
+  var scrollY = window.pageYOffset;
+
+  ramadan.style.top = scrollY * 0.6 + "px";
+  crescent.style.top = scrollY * 0.5 + "px";
+  crescent.style.right = -scrollY * 0.2 + "px";
+});
+
+//happy ramadan everyone ♥
